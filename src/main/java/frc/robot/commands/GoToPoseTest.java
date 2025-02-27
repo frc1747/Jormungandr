bd12147dcf45b6b76b60fad0a325b59094a4691f
Elliot Holmes <elliot.b.holmes@gmail.com>
2025-02-27 18:45:54 -0500

    got fully functional translational part of GoToPoseTest

--- src/main/java/frc/robot/commands/GoToPoseTest.java
+++ src/main/java/frc/robot/commands/GoToPoseTest.java
@@ -18,7 +18,7 @@ public class GoToPoseTest extends Command {
     Pose2d desiredPose;
     Transform2d difference;
     double diffTransMagnitude;
-  
+
   /** Creates a new GoToPoseTest. */
   public GoToPoseTest(PoseEstimatorSubsystem poseEstimator, Drivetrain drivetrain, Pose2d desiredPose) {
     this.poseEstimator = poseEstimator;
@@ -36,27 +36,26 @@ public class GoToPoseTest extends Command {
     // magnitude in meters of difference between current estimated pose of robot and the desired pose
     diffTransMagnitude = Math.sqrt(Math.pow(difference.getX(), 2) + Math.pow(difference.getY(), 2));
   }
-
+  
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // transform2d that represents difference between current estimated pose and desired pose
-    difference = poseEstimator.getEstimatedPose().minus(desiredPose);
-    
+    difference = desiredPose.minus(poseEstimator.getEstimatedPose());
+
     // distance to desired pose translation in meters
     diffTransMagnitude = Math.sqrt(Math.pow(difference.getX(), 2) + Math.pow(difference.getY(), 2));
-    
+
     // velocityMultiplier [0.01, 1] tells robot how quickly to move towards desired pose translation
     double velocityMultiplier = (Math.sqrt(diffTransMagnitude / 1)); // starts slowing down within 1 meter
     if (velocityMultiplier > 1) velocityMultiplier = 1;
     if (velocityMultiplier < 0.05) velocityMultiplier = 0.05;
 
     // full speed translation in the direction of the desired pose translation
-    Translation2d driveTranslation = new Translation2d(1, new Rotation2d(Math.atan(difference.getY()/difference.getX())));
-
+    Translation2d driveTranslation = new Translation2d(1, new Rotation2d(Math.atan2(difference.getY(),difference.getX())));
     drivetrain.simpleDrive(driveTranslation.times(velocityMultiplier), 0);
   }
- 
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
